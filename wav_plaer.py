983bef45003773c4deb80259475168ec60813f95
tozawahiroya <115676510+tozawahiroya@users.noreply.github.com>
2023-02-19 21:57:23 +0900

    Update wav_plaer.py

--- wav_plaer.py
+++ wav_plaer.py
@@ -1,11 +1,13 @@
 
 import streamlit as st
-import io
+#import io
 
-speech_file='test_2023-02-19-06-40-28-929818.wav'
-with io.open(speech_file,'rb') as f:
-    content = f.read()
+#speech_file='test_2023-02-19-06-40-28-929818.wav'
+#with io.open(speech_file,'rb') as f:
+#    content = f.read()
 
+content = st.file_uploader(label)
+    
 st.audio(content)
 
 
